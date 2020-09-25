f634ff64e90d439cadd41d2681c38b3fe6af8911
tandat198 <tandat198@gmail.com>
2020-09-25 21:12:53 +0700

    update

--- routes/api/test/index.js
+++ routes/api/test/index.js
@@ -1,10 +1,12 @@
 const router = require("express").Router();
 const testController = require("./controller");
 const { authenticate, authorize, checkToken } = require("../../../middlewares/auth");
+const { uploadSingle } = require("../../../middlewares/upload");
 
 router.get("/", checkToken, testController.getTests);
 router.get("/:testId", checkToken, testController.getTestById);
 router.post("/", authenticate, authorize(["admin"]), testController.createTest);
+router.post("/upload-image", uploadSingle("image"));
 router.put("/:testId", authenticate, authorize(["admin"]), testController.updateTest);
 router.patch("/:testId", authenticate, authorize(["admin"]), testController.updateIsPublicOfTest);
 
--- routes/api/word/index.js
+++ routes/api/word/index.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 const wordController = require("./controller");
 const { authenticate, authorize } = require("../../../middlewares/auth");
+const { uploadSingle } = require("../../../middlewares/upload");
 
 router.get("/get-words-by-text", wordController.getWordsByText);
 router.post("/", authenticate, authorize(["admin"]), wordController.createWord);
+router.post("/upload-image", uploadSingle("image"));
 router.put("/", authenticate, authorize(["admin"]), wordController.updateWord);
 
 module.exports = router;

f35c9720b2f6091ab6fbccb0f6d10676adb9f5a4
tandat198 <tandat198@gmail.com>
2020-09-27 15:53:46 +0700

    update

--- routes/api/test/controller.js
+++ routes/api/test/controller.js
@@ -61,9 +61,9 @@ const getTestById = async (req, res) => {
             const idOfWordsInQuestions = questions
                 .filter((ques) => (ques.word ? true : false))
                 .map((ques) => ques.word._id);
-            const savedWords = await SavedWord.find({ user: user.id, word: { $in: idOfWordsInQuestions } }).map(
-                (word) => word._id
-            );
+            const savedWords = await SavedWord.find({ user: user.id })
+                .filter((savedWord) => idOfWordsInQuestions.includes(savedWord.word))
+                .map((word) => word._id);
             if (foundResult) {
                 return res.status(200).json({ test: test.transform(), result: foundResult.transform(), savedWords });
             } else {

93f62f7c166aad12d5d4865232683c3c497347da
tandat198 <tandat198@gmail.com>
2020-09-27 15:57:07 +0700

    update socket

--- routes/api/test/controller.js
+++ routes/api/test/controller.js
@@ -61,6 +61,7 @@ const getTestById = async (req, res) => {
             const idOfWordsInQuestions = questions
                 .filter((ques) => (ques.word ? true : false))
                 .map((ques) => ques.word._id);
+            console.log(idOfWordsInQuestions);
             const savedWords = await SavedWord.find({ user: user.id })
                 .filter((savedWord) => idOfWordsInQuestions.includes(savedWord.word))
                 .map((word) => word._id);
